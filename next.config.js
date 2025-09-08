b9589cb4a558239287686e934c6233ea0da1888d
afandi1 <afandi@prakerja.go.id>
2025-09-09 01:23:44 +0700

    test assets

--- next.config.js
+++ next.config.js
@@ -4,12 +4,9 @@ const base = isGh ? "/cv" : "";
 
 module.exports = {
   output: "export",
-<<<<<<< HEAD
   images: { unoptimized: true },
   basePath : '/cv',                          // /cv di prod, "" di lokal
   assetPrefix: isProd ? `/${repo}/` : undefined,
-=======
->>>>>>> 51bfafa0dddfb5433dead7cc31fbb316bea277bc
   trailingSlash: true,
   images: { unoptimized: true },
   basePath: base,
