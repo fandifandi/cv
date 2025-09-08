51bfafa0dddfb5433dead7cc31fbb316bea277bc
afandi1 <afandi@prakerja.go.id>
2025-09-09 00:30:06 +0700

    update site path

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 // next.config.js
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const repo = process.env.NEXT_PUBLIC_GHPAGES_REPO || "cv";   // bisa override via env
-const prodBase = `/${repo}`;
+const isGh = process.env.GHPAGES === "1";   // flag sederhana
+const base = isGh ? "/cv" : "";
 
 module.exports = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-
-  // -> Local: ""  |  GitHub Pages (project): "/cv"
-  basePath: isProd ? prodBase : "",
-  assetPrefix: isProd ? `${prodBase}/` : undefined,
-
-  // bikin mudah dipakai di client/helper
-  env: { NEXT_PUBLIC_BASE_PATH: isProd ? prodBase : "" },
-
-  // optional quality-of-life:
-  productionBrowserSourceMaps: false,
+  basePath: base,
+  assetPrefix: base ? base + "/" : undefined,
 };
